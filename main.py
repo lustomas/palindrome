7172283db67ab9cb7af2fcc7248b658ac3ee4b1c
Tomasz Luszczak <luszczaktomasz@gmail.com>
2022-04-04 16:48:31 +0200

    Add main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+def isPalindrome(a):
+    """
+        Checks if string 'a' is a palindrome or not
+        Returns True if yes
+        Returns False if no
+    """
+    if a == a[::-1]:
+        return True
+    elif a != a[::-1]:
+        return False
+
+a = "rabarbar"
+
+print(isPalindrome(a))
